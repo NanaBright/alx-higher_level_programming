1273f59538e0738efc37d9b4cda118a65d603bd3
Bright <nanabryte.nb@gmail.com>
2023-03-27 16:21:46 -0700

    tasks-Vi

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    #prc - variable to be initialised/used
     prc = 0
+
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except IndexError:
+        except:
             break
         else:
             prc += 1
 
     print()
-    return prc
+    return (prc)
