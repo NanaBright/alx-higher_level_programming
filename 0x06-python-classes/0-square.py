ed84321208e7d93ce93d3813c00ec632f82dfdbf
Bright <nanabryte.nb@gmail.com>
2023-03-28 17:28:24 -0700

    tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 class Square:
-    """class square represents a square."""
+    """Class Square that defines a square.
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square defines a square object
+    """Class Square that defines a square object
     """
     def __init__(self, size):
-        """start with a method that stores the size of the square
+        """Initialize method that stores the size of the square
+        Args:
+            param1 (int): size of the square
         """
         self.__size = size
