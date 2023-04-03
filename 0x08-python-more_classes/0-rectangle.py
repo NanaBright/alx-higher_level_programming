5cfcf107b302bfb6f17d9965f00e24bb3f23ec96
Bright <nanabryte.nb@gmail.com>
2023-04-03 09:42:06 -0700

    tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ Define an empty class called Rectangle that represents a rectangle """
+
+    pass
+    """ Use the pass statement to indicate that this class doesn't have any attributes or methods yet """
