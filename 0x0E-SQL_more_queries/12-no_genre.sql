3d2de0a453b296dd76a30aae7116461a6d5e7704
Bright <nanabryte.nb@gmail.com>
2023-05-17 16:13:37 -0700

    tasks

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Script to list all shows in hbtn_0d_tvshows
+SELECT tv_shows.title, tsg.genre_id
+	FROM tv_shows
+	LEFT JOIN tv_show_genres AS tsg
+	ON tv_shows.id = tsg.show_id
+	WHERE tsg.genre_id IS NULL
+	ORDER BY tv_shows.title ASC, tsg.genre_id ASC;
