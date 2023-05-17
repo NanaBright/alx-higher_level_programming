e8dbff6eff2a6749a758cf7b58494d527d13810e
Bright <nanabryte.nb@gmail.com>
2023-05-17 14:58:28 -0700

    tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---Script that lists all the priviledges of MySQL users/localhost
+--Script that lists all the priviledges of MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
