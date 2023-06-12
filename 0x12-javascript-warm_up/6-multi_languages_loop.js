35b0007a20f5cfd66b0a67fc5424b5cbcd450113
Bright <nanabryte.nb@gmail.com>
2023-06-12 16:34:03 -0700

    tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+const m = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(msg[i]);
+  console.log(m[i]);
 }
